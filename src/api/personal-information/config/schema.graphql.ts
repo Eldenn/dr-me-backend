3b9a3a058a1cf0edadc535aaf97c9897b11ddfd6
Matthieu Chane Wen Ching <matthieu.chanewenc@gmail.com>
2023-03-04 23:58:13 +0100

    [GRAPHQL] add mutation to personalInformation

--- src/api/personal-information/config/schema.graphql.ts
+++ src/api/personal-information/config/schema.graphql.ts
@@ -1,24 +1,40 @@
 export default {
   definition: ``,
   typeDefs: `
-    type Query {
+    extend type Query {
       myPersonalInformation: PersonalInformation
     }
-  `,
-  query: `
-    type Query {
-      myPersonalInformation: PersonalInformation
+
+    extend type Mutation {
+      updateMyPersonalInformation(input: PersonalInformationInput): PersonalInformation
     }
   `,
   type: {},
   resolvers: {
     Query: {
       myPersonalInformation: {
-        resolve: async () => {
-          const ctx = strapi.requestContext.get();
+        resolve: async (obj, options, ctx) => {
           const { user } = ctx.state;
-          const personalInformation = await strapi.entityService.findOne('api::personal-information.personal-information', 1, {
-            user: user.id,
+
+          const personalInformations = await strapi.query("api::personal-information.personal-information").findOne({
+            where: {
+              user: user.id
+            }
+          });
+
+          return personalInformations;
+        },
+      },
+    },
+    Mutation: {
+      updateMyPersonalInformation: {
+        resolve: async (obj, options, ctx, data) => {
+          const { input } = data.variableValues;
+          const { user } = ctx.state;
+
+          const personalInformation = await strapi.db.query("api::personal-information.personal-information").update({
+            where: { user: user.id },
+            data: input,
           });
 
           return personalInformation;
